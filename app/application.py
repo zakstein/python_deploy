38ce36b6db5213844f9ec63f6d92d2ccaf72c039
Zak Stein <zak@box.com>
2014-11-08 11:40:10 -0800

    Static and media files are served correctly
    Nginx is reading the correct locations for the media and static files.

--- app/application.py
+++ app/application.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-	return "HELLOWURLD"
+def root_page():
+	return render_template('index.html')
 
 
 if __name__ == "__main__":
